22a2677ce8544083ea31be037474ae59fb420c87
Martijn van Beers <mail_dev@martijn.at>
2022-12-22 13:27:33 +0100

    Add bertviz's neuronview changes

--- src/transformers/models/bert/modeling_bert.py
+++ src/transformers/models/bert/modeling_bert.py
@@ -343,7 +343,15 @@ class BertSelfAttention(nn.Module):
         new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
         context_layer = context_layer.view(*new_context_layer_shape)
 
-        outputs = (context_layer, attention_probs) if output_attentions else (context_layer,)
+        if output_attentions:
+            attn_data = {
+                    'attn': attention_probs,
+                    'queries': query_layer,
+                    'keys': key_layer,
+                }
+            outputs = (context_layer, attn_data,)
+        else:
+            outputs = (context_layer,)
 
         if self.is_decoder:
             outputs = outputs + (past_key_value,)
@@ -862,6 +870,7 @@ class BertModel(BertPreTrainedModel):
         super().__init__(config)
         self.config = config
 
+        config.output_attentions = True
         self.embeddings = BertEmbeddings(config)
         self.encoder = BertEncoder(config)
 
